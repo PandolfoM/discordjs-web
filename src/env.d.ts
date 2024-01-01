6407899d3a8674393f2a8b2e03000e66917454c9
Matthew Pandolfo <matt@pandolfo.com>
2024-01-01 15:09:15 -0500

    add firebase

---
+++ src/env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+  readonly APIKEY: string;
+  readonly AUTHDOMAIN: string;
+  readonly PROJECTID: string;
+  readonly STORAGEBUCKET: string;
+  readonly MESSAGINGSENDERID: string;
+  readonly APPID: string;
+}
